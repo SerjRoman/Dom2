3c4a607de9d536294c7338fd56aaf6b2720942c0
Denis <vibid567@gmail.com>
2022-08-26 20:04:21 +0300

    ДенисМ

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import turtle
 
 mainTurtle = turtle.Turtle()
-
-
-
+def denis(x,y):
+    mainTurtle.penup()
+    mainTurtle.goto(x,y)
+    mainTurtle.pendown()
+    mainTurtle.fillcolor("blue")
+    mainTurtle.begin_fill()
+    mainTurtle.forward(100)
+    mainTurtle.right(90)
+    mainTurtle.forward(50)
+    mainTurtle.right(90)
+    mainTurtle.forward(100)
+    mainTurtle.right(90)
+    mainTurtle.forward(50)
+    mainTurtle.end_fill()
+denis(100,100)
 mainTurtle.hideturtle()
+
 turtle.exitonclick()
