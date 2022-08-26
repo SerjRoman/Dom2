7a1003833f4e03370cdb86541e4ecc58bad4e204
Maks <maksghittk@gmail.com>
2022-08-26 20:26:43 +0300

    maxsim1

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import turtle
-
 mainTurtle = turtle.Turtle()
 
-
+def maxsim(x,y):
+    mainTurtle.pencolor('orange')
+    mainTurtle.penup()
+    mainTurtle.goto(-100, -100)
+    mainTurtle.pendown()
+    mainTurtle.fillcolor('orange')
+    mainTurtle.begin_fill
+    mainTurtle.left(60)
+    mainTurtle.forward(200)
+    mainTurtle.right(120)
+    mainTurtle.forward(200)
+    mainTurtle.right(120)
+    mainTurtle.forward(200)
+    mainTurtle.end_fill
 
 mainTurtle.hideturtle()
 turtle.exitonclick()
